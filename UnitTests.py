0e523c9cd378024f2a51494bb5a03fcc06b54cc7
Connor Jacobs <connorjacobs@Connors-MacBook-Pro-2.local>
2024-04-05 10:58:53 -0700

    capitlization

--- UnitTests.py
+++ UnitTests.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from toDoList import ToDoList
+from ToDoList import ToDoList
 
 class TestToDoList(unittest.TestCase):
     def setUp(self):
