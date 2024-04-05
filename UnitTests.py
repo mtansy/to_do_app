9e27efe957496f4227e5945838239371c7de9420
Connor Jacobs <connorjacobs@Connors-MacBook-Pro-2.local>
2024-04-05 11:13:11 -0700

    let me rename pls

--- UnitTests.py
+++ UnitTests.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from ToDo import ToDoList
+from to_do import ToDoList
 
 class TestToDoList(unittest.TestCase):
     def setUp(self):
