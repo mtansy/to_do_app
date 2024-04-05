e6811c3cf6a13e3802ff1bba463286f8523e327f
Connor Jacobs <connorjacobs@Connors-MacBook-Pro-2.local>
2024-04-05 10:52:40 -0700

    pylint

--- toDoList.py
+++ toDoList.py
@@ -1,15 +1,44 @@
+"""
+This module defines a simple ToDoList class.
+"""
+
 class ToDoList:
+    """
+    A simple class to manage a ToDo list.
+    """
+
     def __init__(self):
+        """
+        Initialize a new ToDoList instance.
+        """
         self.tasks = []
 
     def add_task(self, task):
+        """
+        Add a task to the ToDo list.
+
+        Args:
+            task (str): The task to add.
+        """
         self.tasks.append(task)
 
     def delete_task(self, task):
+        """
+        Delete a task from the ToDo list.
+
+        Args:
+            task (str): The task to delete.
+        """
         if task in self.tasks:
             self.tasks.remove(task)
         else:
             print("Task not found!")
 
     def get_tasks(self):
+        """
+        Get the list of tasks in the ToDo list.
+
+        Returns:
+            list: The list of tasks.
+        """
         return self.tasks
